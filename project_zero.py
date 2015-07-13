dcb60b8e285bed390d589037d8434a7e0d0c359f
Richard Fagervoll <shaitan82@gmail.com>
2015-07-13 20:29:42 +0200

    Added link to CodeSkulptor version of the program.

--- project_zero.py
+++ project_zero.py
@@ -3,4 +3,7 @@
 # Modify the print statement according to 
 # the mini-project instructions
 
+#CodeSkulptor link:
+#http://www.codeskulptor.org/#user40_lXiJqEZDdrSdSu5.py
+
 print "We want... a shrubbery!"
