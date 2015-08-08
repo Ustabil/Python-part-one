cc4736929e95e97e995e67becaef32561d73f672
Richard Fagervoll <shaitan82@gmail.com>
2015-08-08 23:27:33 +0200

    Step one, added timer

--- mini_projects/stopwatch_the_game/stopwatch_the_game.py
+++ mini_projects/stopwatch_the_game/stopwatch_the_game.py
@@ -1,7 +1,8 @@
 # template for "Stopwatch: The Game"
-
+import simplegui
 # define global variables
 
+interval = 100
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -12,8 +13,8 @@ def format(t):
 
 
 # define event handler for timer with 0.1 sec interval
-
-
+def timer_handler():
+	print ".1 sec!"
 # define draw handler
 
     
@@ -21,9 +22,9 @@ def format(t):
 
 
 # register event handlers
-
+timer = simplegui.create_timer(interval, timer_handler)
 
 # start frame
-
+timer.start()
 
 # Please remember to review the grading rubric
