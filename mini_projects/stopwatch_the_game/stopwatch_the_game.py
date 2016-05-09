a7425d030353555479cc00ab1c12973645174919
Richard Fagervoll <shaitan82@gmail.com>
2016-05-09 14:38:15 +0200

    finished the stopwatch game

--- mini_projects/stopwatch_the_game/stopwatch_the_game.py
+++ mini_projects/stopwatch_the_game/stopwatch_the_game.py
@@ -4,28 +4,44 @@ import simplegui
 
 interval = 100
 stopwatch = 0
+stops = 0
+hits = 0
+
+
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 def format(t):
-	minutes = t % 600
+	minutes = t / 600
+	seconds = (t % 600) / 10
     tenths = t % 10
-    seconds = (t - (tenths)) / 10
+
 
     if seconds < 10 :
     	return str(minutes) + ":0" + str(seconds) + "." + str(tenths)
     if seconds >= 10:
     	return str(minutes) + ":" + str(seconds) + "." + str(tenths)
-    
+
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start_button_handler():
 	timer.start()
 def stop_button_handler():
+	global stops, hits, stopwatch
+	# add code to check if t % 0 == 0
+	if timer.is_running():
+		stops += 1
+		if stopwatch % 10 == 0:
+			hits += 1
+
 	timer.stop()
+
+
 def reset_button_handler():
-	global stopwatch
+	global stopwatch, stops, hits
 	timer.stop()
 	stopwatch = 0
+	stops = 0
+	hits = 0
 
 # define event handler for timer with 0.1 sec interval
 def timer_handler():
@@ -34,8 +50,9 @@ def timer_handler():
 
 # define draw handler
 def draw_handler(canvas):
-    canvas.draw_text(str(format(stopwatch)), (200, 220), 36, 'Red')
-    
+    canvas.draw_text(str(format(stopwatch)), (175, 220), 36, 'Red')
+	canvas.draw_text(str(hits) + "/" + str(stops), (300, 40), 36, 'White')
+
 # create frame
 frame = simplegui.create_frame('Stopwatch', 400, 400)
 
